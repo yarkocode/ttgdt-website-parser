9361a45a8a4ad782a30cf332d5ce779924f4053f
yarkocode <yao6lav.velicko@gmail.com>
2025-10-02 15:22:08 +0700

    dev(parse): test for raise unavailable date select exception

--- tests/test_lessons_parsing.py
+++ tests/test_lessons_parsing.py
@@ -1,12 +1,12 @@
 import os.path
-from datetime import datetime
+from datetime import datetime, timedelta
 from unittest import mock
 from unittest.mock import AsyncMock
 
 import pytest
 
 from ttgdtparser.constants import raspisanie_zanyatij
-from ttgdtparser.exceptions import WebsiteUnavailableException
+from ttgdtparser.exceptions import WebsiteUnavailableException, NoTimetableAvailablePerDate
 from ttgdtparser.parser import parse_lessons
 
 
@@ -86,3 +86,36 @@ async def test_lesson_parsing_website_unavailable() -> None:
             await parse_lessons(raspisanie_zanyatij(base=True), '121,123,132', datetime.now())
 
         print("Successful exception: {}".format(e))
+
+
+@pytest.mark.asyncio
+async def test_lesson_parsing_with_wrong_date() -> None:
+    if not file_exists("lessons.html"):
+        pytest.skip("lessons.html not found in extra/")
+
+    with open("extra/lessons.html", mode='r', encoding='utf-8') as lessons_file:
+        html = lessons_file.read()
+
+    mock_response = mock.Mock()
+    mock_response.status = 200
+    mock_response.text = mock.AsyncMock(return_value=html)
+    mock_response.__aenter__ = mock.AsyncMock(return_value=mock_response)
+    mock_response.__aexit__ = mock.AsyncMock(return_value=None)
+
+    mock_session = mock.Mock()
+    mock_session.post.return_value = mock_response
+    mock_session.__aenter__ = AsyncMock(return_value=mock_session)
+    mock_session.__aexit__ = AsyncMock(return_value=None)
+
+    now = datetime.now()
+    sunday_delta = 6 - now.weekday() % 7
+    now += timedelta(days=sunday_delta)
+
+    with mock.patch('ttgdtparser.parser.ClientSession', return_value=mock_session):
+        with pytest.raises(NoTimetableAvailablePerDate):
+            try:
+                await parse_lessons(raspisanie_zanyatij(base=True), '121,123,132', now)
+            except WebsiteUnavailableException:
+                pytest.fail("Mocked website response is wrong")
+
+        print("Successful exception: {}".format(mock_response.text))
