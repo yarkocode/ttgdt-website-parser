a192270999e9d7ff8c198eac4c9370957e2ea533
yarkocode <yao6lav.velicko@gmail.com>
2025-10-02 16:32:08 +0700

    dev(tests): fix accumulation function for available indexes

--- src/ttgdtparser/accumulator.py
+++ src/ttgdtparser/accumulator.py
@@ -9,11 +9,16 @@ def accumulate_lessons(lessons: list[Lesson], changes: List[Change]) -> list[Les
     for change in changes:
         if change.index_is_time:
             result.append(change)
+            continue
 
         if change.discipline.lower() == 'нет':
             continue
 
         selected_indexes = list(filter(lambda l: l.index in change.subindexation, lessons))
+
+        if len(selected_indexes) == 0:
+            result.append(change)
+            continue
 
         for lesson in selected_indexes:
             lesson.change = change
@@ -22,5 +27,8 @@ def accumulate_lessons(lessons: list[Lesson], changes: List[Change]) -> list[Les
                 change.discipline = lesson.discipline
 
             result.append(lesson)
+
+    unfilled = list(filter(lambda l: l.index not in [l.index for l in changes], lessons))
+    result.extend(unfilled)
 
     return result
